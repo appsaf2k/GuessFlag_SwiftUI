315f3e83cb045b3286ea758f3bfb11a3c86d056d
appsaf2k <appsaf2k@gmail.com>
2022-08-27 09:34:47 +0300

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by @andreev2k on 27.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
